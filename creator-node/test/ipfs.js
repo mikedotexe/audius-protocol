85834d1d27b899fc3c73861356b95b946117df7c
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2020-07-21 15:15:46 -0700

    lint

--- creator-node/test/ipfs.js
+++ creator-node/test/ipfs.js
@@ -2,7 +2,6 @@ const ipfsClientLatest = require('ipfs-http-client-latest')
 const ipfsLatest = ipfsClientLatest({ host: 'localhost', port: '5001', protocol: 'http' })
 
 describe('test ipfs', function () {
-
   it.only('creates ipfs connection', async function () {
     console.log(await ipfsLatest.id())
   })
