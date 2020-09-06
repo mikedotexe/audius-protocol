b088f74e49a383861107727f15093bec36e93079
Mike Purvis <mikedotexe@gmail.com>
2020-09-06 09:03:44 -0700

    modify truffle-config adding nearTestnet and provider

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -4,6 +4,24 @@
  * @version 0.0.1
  */
 
+const { NearProvider, nearlib: nearAPI } = require('near-web3-provider');
+// Because we're using a function-call access key, this is the same as the NEAR_LOCAL_EVM
+const NEAR_LOCAL_ACCOUNT_ID = 'audius.demo.testnet';
+const NEAR_LOCAL_NETWORK_ID = 'default';
+const NEAR_LOCAL_URL = 'https://rpc.testnet.near.org';
+const NEAR_EXPLORER_URL = 'https://explorer.testnet.near.org';
+const NEAR_LOCAL_EVM = 'evm.demo.testnet';
+
+function NearTestNetProvider(keyStore) {
+  return new NearProvider({
+    nodeUrl: NEAR_LOCAL_URL,
+    keyStore,
+    networkId: NEAR_LOCAL_NETWORK_ID,
+    masterAccountId: NEAR_LOCAL_ACCOUNT_ID,
+    evmAccountId: NEAR_LOCAL_EVM,
+  });
+}
+
 // Import babel for ES6 support
 require('babel-register')({
   presets: [
@@ -21,6 +39,11 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
+    nearTestnet: {
+      network_id: "*",
+      skipDryRun: true,
+      provider: () => NearTestNetProvider(),
+    },
     development: {
       host: '127.0.0.1',
       port: 8545,
